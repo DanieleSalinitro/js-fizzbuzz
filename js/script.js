4fafbee5bff4f4c13493aec9a1cdb0ae3935dc8c
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-03-09 20:23:49 +0100

    COMPLETATO MA CON PARTE AGGIUNTIVA NON FUNZIONANTE

--- js/script.js
+++ js/script.js
@@ -8,6 +8,11 @@ title.className = 'text-center p-5 fw-bold text-white';
 title.innerHTML='FizzBuzzDOM';
 let chessboard = document.createElement('div');
 chessboard.setAttribute('id', 'chessboard')
+let prova
+//creazione scacco tramite js
+
+//let htmlboxes1 = document.createElement('div');
+//htmlboxes1.className = 'ds-box fw-bold';
 
 //inizio ciclo e generazione scacchi
 for (i=1; i<=numbox; i++){
@@ -18,12 +23,28 @@ for (i=1; i<=numbox; i++){
     } else if (i % 5 === 0) {
         htmlboxes += `<div class="ds-box box5">buzz</div>`;
     } else {
-        htmlboxes += `<div class="ds-box">${i}</div>`;
+        htmlboxes += `<div class="ds-box fw-bold">${i}</div>`;
     }
 }
+/*PROVA NON FUNZIONANTE
+for (i=1; i<=numbox; i++){
+    if (i % 15 === 0){
+        htmlboxes1.className = 'ds-box fw-bold box15';
+        htmlboxes1+= htmlboxes1.innerHTML = `BuzzFizz`;
+    } else if (i % 3 === 0){
+        htmlboxes1.className = 'ds-box fw-bold box3';
+        htmlboxes1+= htmlboxes1.innerHTML = `Fizz`;
+    } else if (i % 5 === 0) {
+        htmlboxes1.className = 'ds-box fw-bold box5';
+        htmlboxes1+= htmlboxes1.innerHTML = `Buzz`;
+    } else {
+        htmlboxes1+= htmlboxes1.innerHTML = i;    
+    }
+}*/
 
 //inserisco la generazione degli scacchi al div chessboard
 chessboard.innerHTML=htmlboxes;
 
 //inserisco nel file index.html il titolo e il div chessboard appena riempito
 document.getElementById('wrapper').append(title, chessboard);
+
